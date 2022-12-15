a3d1a1b9acfaf347546fd819fc876982d60fd210
MaRtin322s <marti.sofroniev0408@gmail.com>
2022-12-15 12:09:45 +0200

    add regExp

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import "./App.css";
+import React, { useState, useMemo } from "react";
 
 function App() {
-  return (
-    <div className="App">
-     <div className="control has-icons-right">
-        <input
-          className="input is-large"
-          type="text"
-          placeholder="Enter number..."
+    const [text, setText] = useState('');
+    const checkIfValid = (string) => {
+        const numbers = /^\d+$/;
+        return numbers.test(string);
+    };
 
-        />
-        <span className="icon is-small is-right">
-          <i className="fas fa-times" />
-        </span> 
-      </div>
-    </div>
-  );
+    const validator = useMemo(() => checkIfValid(text));
+
+    return (
+        <div className="App">
+            <div className="control has-icons-right">
+                <input
+                    className="input is-large"
+                    type="text"
+                    placeholder="Enter number..."
+                    value={text}
+                    onChange={(e) => {
+                        setText(e.target.value);
+                    }}
+
+                />
+                <span className="icon is-small is-right">
+                    <i className={validator ? "fa-check" : "fas fa-times"} />
+                </span>
+            </div>
+        </div>
+    );
 }
 
 export default App;
