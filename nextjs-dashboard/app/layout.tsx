6cfca9e18f693c6321368fdd674185b43dc7902b
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-05-20 11:49:02 +0300

    css modules and global css

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,3 +1,5 @@
+import '@/app/ui/global.css';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <h1 className="text-blue-500">I'm blue!</h1>
+        <div
+          className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+        />
+        {children}
+      </body>
     </html>
   );
 }
