1994e839e93727392c560fe5a34864d09e9dd1cc
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-05-20 12:02:31 +0300

    add custom fonts and images

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/ui/global.css';
+import { inter } from '../app/ui/font';
 
 export default function RootLayout({
   children,
@@ -7,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${inter.className} antialiased`}>
         <h1 className="text-blue-500">I'm blue!</h1>
         <div
           className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
